4644dda894b998c0c26f61875c4e5ae2c5569498
LeilaC1 <leilaclesca13@gmail.com>
2023-10-23 10:23:32 -0400

    added output and int

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,7 @@ console.log("start index.js");
 window.onload = init;
 
 function init(){
-    console.log("start init.")
-
+    console.log("start init")
 
 const AddBtn = document.getElementById("AddBtn");
 AddBtn.onclick = onAddBtnClicked;
@@ -16,38 +15,40 @@ SubtractBtn.onclick = onSubtractBtnClicked;
 const MultiplyBtn = document.getElementById("MultiplyBtn");
 MultiplyBtn.onclick = onMultiplyBtnClicked;
 
-const DivideBtn =document.getElementById("DivideBtn");
+const DivideBtn = document.getElementById("DivideBtn");
 DivideBtn.onclick = onDivideBtnClicked;
-console.log("end init.")
+console.log("end init")
+
+
+
 }
 function onAddBtnClicked (){
     console.log("btn clicked");
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
-    let numberValue = number1Field.value + number2Field.value;
-    let answer = numberValue;
-   // const answerField = document.getElementById(answerField);
- //   answerField.innerHTML = answer;
+    let numberValue = parseInt(number1Field.value) + parseInt(number2Field.value);
+    const answerField = document.getElementById("answerField");
+    answerField.innerHTML = numberValue;
 }
 function onSubtractBtnClicked (){
     console.log("btn clicked");
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
     let numberValue = number1Field.value - number2Field.value;
-    //let answer = numberValue;
- //   alert(message);
+    const answerField = document.getElementById("answerField");
+    answerField.innerHTML = numberValue;
 }function onMultiplyBtnClicked (){
     console.log("btn clicked");
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
     let numberValue = number1Field.value * number2Field.value;
-  //  let answer = numberValue;
- //   alert(message);
+    const answerField = document.getElementById("answerField");
+    answerField.innerHTML = numberValue;
 }
 function onDivideBtnClicked (){
     const number1Field = document.getElementById("number1Field");
     const number2Field = document.getElementById("number2Field");
     let numberValue = number1Field.value / number2Field.value;
-  //  let answer = numberValue;
-  //  alert(message);
+    const answerField = document.getElementById("answerField");
+    answerField.innerHTML = numberValue;
 }
